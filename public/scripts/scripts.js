ad68614422092289f49362a4e633f86ba32c2265
SolJCam <scameron110@gmail.com>
2015-05-16 04:08:49 -0400

    styled and standardized chat view and functionality

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,12 +1,21 @@
 $(document).ready(function(){
 	console.log("scripts are being read")
-	var socket = io();
-	$('form').submit(function(){
-		socket.emit('chat message', $('#chat').val());
-		$('#chat').val('');
-		return false
-	});
-	socket.on('chat message', function(msg){
-		$('#messages').append($('<li>').text(msg));
+
+	$('#modal1').openModal();
+
+	$('a').on('click', function(){
+		username = $('<p>').append($('input[type=text]').val()).css("color", "red")
+		name = $('input[type=text]').val()
+
+		var socket = io();
+		$('form').submit(function(){
+			socket.emit('chat message', $('#chat').val());
+			$('#chat').val('');
+			return false
+		});
+		socket.on('chat message', function(msg){
+			$('#messages').append($('<li>').text(name+' '+msg));
+			$('#mine').scrollTop($('#messages').height());
+		});
 	});
 })
