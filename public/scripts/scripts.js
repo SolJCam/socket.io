ef3af5d4e4f14702a997ff07c1c694d595ccf3ff
SolJCam <scameron110@gmail.com>
2015-05-18 23:07:15 -0400

    jQuery logic to add username and avatar

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -4,8 +4,30 @@ $(document).ready(function(){
 	$('#modal1').openModal();
 
 	$('a').on('click', function(){
-		// username = $('<p>').append($('input[type=text]').val()).css("color", "red")
 		name = $('input[type=text]').val()
+
+		img1 = $('input[type=url]').val()
+// Accessing file objects representing the files selected by the user : https://developer.mozilla.org/en-US/docs/Using_files_from_web_applications
+		img = $('input[type=file]')[0].files[0]
+		imgEle = $('<img>')
+		ok = ""
+
+// Instance of FileReader object that asynchronously reads contents of files stored on the user's computer : https://developer.mozilla.org/en-US/docs/Web/API/FileReader
+		reader = new FileReader();
+		 												 
+// Event handler that returns file data in the form of a URL
+		reader.onloadend = function () {
+			imgEle.src = reader.result;
+			ok = imgEle.src
+			console.log(ok)
+		}
+
+		if (img){
+			ok = reader.readAsDataURL(img);
+		} else {
+			imgEle.src = "";
+		}
+		
 
 		var socket = io();
 		$('form').submit(function(){
@@ -14,8 +36,15 @@ $(document).ready(function(){
 			return false
 		});
 		socket.on('chat message', function(msg){
-			$('#messages').append($('<li>').text(name+' '+msg));
-			$('#mine').scrollTop($('#messages').height());
+			if (img) {
+				$('#messages').append($('<li>').append($('<img>').attr('src', ok)).append(name).css("color", "red"));
+			} else if (img1.length > 2) {
+				$('#messages').append($('<li>').append($('<img>').attr('src', img1)).append(name).css("color", "red"));
+			} else {
+				$('#messages').append($('<li>').append($('<img>').attr('src', 'http://i.kinja-img.com/gawker-media/image/upload/s--Z8-FvAOx--/18eyurj9shxzkjpg.jpg')).append(name).css("color", "red"));
+			}
+				$('#messages').append($('<li>').text(msg));
+				$('#mine').scrollTop($('#messages').height());
 		});
 	});
 })
