06dbf97fc19b18af30b06008fc1d8153a1b66428
SolJCam <scameron110@gmail.com>
2020-02-26 20:28:57 -0500

    updates to scripts file and considerable updates to socket.io binary files with package update

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,66 +1,86 @@
 $(document).ready(function(){
 	console.log("scripts are being read")
 
-  $.getJSON('//freegeoip.net/json/?callback=?', function(data){
-    console.log(data.ip)
-    ip = data.ip
-  // debugger
-  })
+	// Using user ip addresses as ids 
+	ip = ''
+	fetch('https://freegeoip.app/json/')
+		.then((response)=>{
+			return response.json();
+		})
+		.then((myJson)=>{
+			ip = myJson.ip;
+			return myJson.ip;
+		})
 
 	$('#modal1').openModal({
 		dismissible: false,
 		ready: function(){
-			    $('#agree').on('click', function(e){
-						name = $('input[type=text]').val()
+			$('#agree').on('click', function(e){
+				name_val = $('input[type=text]').val();
+				name = name_val+'('+ip+')'
 
-						if (name.length == 0){
-				      		e.preventDefault();
-				      		$('#test').append('<p style="color: red; font-family: Source Code Pro">Please provide a username then continue');
-				    	} else {
-							img1 = $('input[type=url]').val();
-					// Accessing file objects representing the files selected by the user : https://developer.mozilla.org/en-US/docs/Using_files_from_web_applications
-							img = $('input[type=file]')[0].files[0]
-              imgId = img.size + ip
-							imgEle = $('<img>');
-							ok = ""
+				debugger
+				let imgLen;
+				let imgEle;
+				let ok;
 
-            // debugger
-					// Instance of FileReader object that asynchronously reads contents of files stored on the user's computer : https://developer.mozilla.org/en-US/docs/Web/API/FileReader
-							reader = new FileReader();
+				if (name.length == 0){
+					
+					e.preventDefault();
+					$('#error').css({"display": "block", "color": "red", "font-family": "Source Code Pro"}).text("Please provide a username then continue");
 
-					// onloadend Event handler that returns file data in the form of a URL
-							reader.onloadend = function () {
-								imgEle.src = reader.result;
-								ok = imgEle.src
-							}
-						// Not entirely sure what's happening here :S
-							if (img){
-								ok = reader.readAsDataURL(img);
-							} else {
-								imgEle.src = ""
-							}
+				} else {
+				// Catching the url value for user image
+					img1 = $('input[type=url]').val();
+				
+				
+				// Accessing file objects representing the image files selected by the user : https://developer.mozilla.org/en-US/docs/Using_files_from_web_applications
+					imgLen = $('input[type=file]')[0].files.length;
+					imgEle = $("<img></img>");
 
-							var socket = io();
-							$('form').submit(function(){
-								socket.emit('chat message', $('#chat').val());
-								$('#chat').val('');
-								return false
-							});
-							socket.on('chat message', function(msg){
-								if (img.size + ip == imgId) {
-									$('#messages').append($('<li>').append($('<img>').attr('src', ok)).append(name).css("color", "red"));
-								} else if (img1.length > 0) {
-									$('#messages').append($('<li>').append($('<img>').attr('src', img1)).append(name).css("color", "red"));
-								} else {
-									$('#messages').append($('<li>').append($('<img>').attr('src', 'http://i.kinja-img.com/gawker-media/image/upload/s--Z8-FvAOx--/18eyurj9shxzkjpg.jpg')).append(name).css("color", "red"));
-								}
-									$('#messages').append($('<li>').text(msg));
-									$('#mine').scrollTop($('#messages').height());
-							});
-				    		$('#modal1').closeModal();
-				    }
-				});
-		}
+				// Instance of FileReader object that asynchronously reads contents of files stored on the user's computer : https://developer.mozilla.org/en-US/docs/Web/API/FileReader
+					reader = new FileReader();
+
+				// onloadend Event handler that returns file data in the form of a URL
+					reader.onloadend = function () {
+						imgEle.src = reader.result;
+						ok = imgEle.src
+					};
+
+					if(imgLen > 0){
+						img = $('input[type=file]')[0].files[0]
+						imgId = img.size
+						// Not entirely sure how this function works :S
+						ok = reader.readAsDataURL(img);
+					} else {
+						// imgEle.src = ""
+						imgId = 'user_'+imgEle
+					}
+
+					var socket = io();
+					$('form').submit(function(){
+						socket.emit('chat message', $('#chat').val());
+						$('#chat').val('');
+						return false
+					});
+					debugger
+
+					socket.on('chat message', function(msg){
+						if (typeof(imgId) === "number") {
+							$('#messages').append($('<li>').append($('<img>').attr('src', ok)).append(name).css("color", "red"));
+						} else if (img1.length > 0) {
+							$('#messages').append($('<li>').append($('<img>').attr('src', img1)).append(name).css("color", "red"));
+						} else {
+							$('#messages').append($('<li>').append($('<img>').attr('src', 'http://i.kinja-img.com/gawker-media/image/upload/s--Z8-FvAOx--/18eyurj9shxzkjpg.jpg')).append(name).css("color", "red"));
+						}
+							$('#messages').append($('<li>').text(msg));
+							$('#mine').scrollTop($('#messages').height());
+					});
+					
+					$('#modal1').closeModal();
+				}
+			});		
+		}		
 	});
 });
 
